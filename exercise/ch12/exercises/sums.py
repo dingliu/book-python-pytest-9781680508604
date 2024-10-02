e1e18a5716f84dea9de3dd828b5e7418f2417c5c
Ding Liu <contact@dingliu.me>
2024-10-01 23:13:50 -0400

    ch12 exercise 1

--- exercise/ch12/exercises/sums.py
+++ exercise/ch12/exercises/sums.py
@@ -1,12 +1,12 @@
 # add the numbers in `data.txt`
+def main():
+    sum = 0.0
+    with open("data.txt", "r") as file:
+        for line in file:
+            number = float(line)
+            sum += number
+    print(f"{sum:.2f}")
 
-sum = 0.0
 
-
-with open("data.txt", "r") as file:
-    for line in file:
-        number = float(line)
-        sum += number
-
-
-print(f"{sum: .2f}")
+if __name__ == "__main__":
+    main()
